c6a0e72769afe8cb02ffa7ab0d9e9be3585f17d6
Tri  Anggi Anggara Saputra <trianggianggara.dev@gmail.com>
2022-06-10 11:17:28 +0700

    first commit

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "chatbot",
+        'USER': "bebekdev",
+        'PASSWORD': "db_password",
+        'HOST': "localhost",
+        'PORT': "5432",  # 5432 by default
     }
 }
 
